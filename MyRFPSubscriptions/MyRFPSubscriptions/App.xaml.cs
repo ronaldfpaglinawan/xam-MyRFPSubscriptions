61558f33482e43c71b852ae9ae7496c7103c0bfa
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2020-04-14 10:39:32 +1200

    Add project files.

---
+++ MyRFPSubscriptions/MyRFPSubscriptions/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MyRFPSubscriptions.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyRFPSubscriptions
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MyRFPSubscriptions/MyRFPSubscriptions/Views/LoginPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyRFPSubscriptions.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterLabel_Tapped(object sender, EventArgs args)
+        {
+            registerStackLayout.IsVisible = true;
+            loginStackLayout.IsVisible = false;
+        }
+
+        private void LoginLabel_Tapped(object sender, EventArgs args)
+        {
+            registerStackLayout.IsVisible = false;
+            loginStackLayout.IsVisible = true;
+        }
+    }
+}
