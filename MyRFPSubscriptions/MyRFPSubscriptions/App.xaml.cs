c99724f3952f92fbe85b464dd684806bf10e9a44
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2020-04-15 21:40:36 +1200

    added SubscriptionsPage and NewSubscriptionPage, setup the app's navigation

--- MyRFPSubscriptions/MyRFPSubscriptions/App.xaml.cs
+++ MyRFPSubscriptions/MyRFPSubscriptions/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MyRFPSubscriptions
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new NavigationPage(new SubscriptionsPage());
         }
 
         protected override void OnStart()
---
+++ MyRFPSubscriptions/MyRFPSubscriptions/Views/SubscriptionsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MyRFPSubscriptions.ViewModels.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyRFPSubscriptions.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SubscriptionsPage : ContentPage
+    {
+        public SubscriptionsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (!Auth.IsAuthenticated())
+            {
+                await Task.Delay(300);
+                await Navigation.PushAsync(new LoginPage());
+            }
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewSubscriptionPage());
+        }
+    }
+}
