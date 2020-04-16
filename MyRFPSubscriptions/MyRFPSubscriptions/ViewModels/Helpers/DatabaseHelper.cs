38f4b535a1abe960c1a7d31e067f6655d33fd55e
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2020-04-16 18:04:33 +1200

    added DatabaseHelper.cs in PCL project, added Firestore.cs in both iOS and Android project

--- MyRFPSubscriptions/MyRFPSubscriptions/ViewModels/Helpers/DatabaseHelper.cs
+++ MyRFPSubscriptions/MyRFPSubscriptions/ViewModels/Helpers/DatabaseHelper.cs
@@ -8,7 +8,7 @@ namespace MyRFPSubscriptions.ViewModels.Helpers
 {
     public interface IFirestore
     {
-        Task<bool> InsertSubscription(Subscription subscription);
+        bool InsertSubscription(Subscription subscription);
         Task<bool> DeleteSubscription(Subscription subscription);
         Task<bool> UpdateSubscription(Subscription subscription);
         Task<IList<Subscription>> ReadSubscription();
@@ -23,7 +23,7 @@ namespace MyRFPSubscriptions.ViewModels.Helpers
             return firestore.DeleteSubscription(subscription);
         }
 
-        public Task<bool> InsertSubscription(Subscription subscription)
+        public bool InsertSubscription(Subscription subscription)
         {
             return firestore.InsertSubscription(subscription);
         }
