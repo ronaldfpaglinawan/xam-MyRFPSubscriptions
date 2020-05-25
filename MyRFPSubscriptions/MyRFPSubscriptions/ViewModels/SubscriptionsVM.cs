f19b7cdeff9a8e7e325af6fb6bcac6c0ccf4b3dd
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2020-05-25 21:06:09 +1200

    created the SubscriptionsDetailPage

--- MyRFPSubscriptions/MyRFPSubscriptions/ViewModels/SubscriptionsVM.cs
+++ MyRFPSubscriptions/MyRFPSubscriptions/ViewModels/SubscriptionsVM.cs
@@ -1,20 +1,42 @@
 ﻿using MyRFPSubscriptions.Models;
 using MyRFPSubscriptions.ViewModels.Helpers;
+using MyRFPSubscriptions.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
 
 namespace MyRFPSubscriptions.ViewModels
 {
-    public class SubscriptionsVM
+    public class SubscriptionsVM : INotifyPropertyChanged
     {
+        private Subscription selectedSubscription;
+
+        public Subscription SelectedSubscription
+        {
+            get 
+            { 
+                return selectedSubscription; 
+            }
+            set 
+            { 
+                selectedSubscription = value;
+                OnPropertyChanged("SelectedSubscription");
+                if (selectedSubscription != null)
+                    App.Current.MainPage.Navigation.PushAsync(new SubscriptionDetailsPage(selectedSubscription));
+            }
+        }
+
+
         public ObservableCollection<Subscription> Subscriptions { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public SubscriptionsVM()
         {
             Subscriptions = new ObservableCollection<Subscription>();
         }
-
+        
         public async void ReadSubscriptions()
         {
             var subscriptions = await DatabaseHelper.ReadSubscriptions();
@@ -24,6 +46,11 @@ namespace MyRFPSubscriptions.ViewModels
             {
                 Subscriptions.Add(s);
             }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ MyRFPSubscriptions/MyRFPSubscriptions/Views/SubscriptionDetailsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MyRFPSubscriptions.Models;
+using MyRFPSubscriptions.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyRFPSubscriptions.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SubscriptionDetailsPage : ContentPage
+    {
+        SubscriptionDetailsVM vm;
+
+        public SubscriptionDetailsPage()
+        {
+            InitializeComponent();
+
+            vm = Resources["vm"] as SubscriptionDetailsVM;
+        }
+
+        public SubscriptionDetailsPage(Subscription selectedSubscription)
+        {
+            InitializeComponent();
+
+            vm = Resources["vm"] as SubscriptionDetailsVM;
+            vm.Subscription = selectedSubscription;
+        }
+    }
+}
